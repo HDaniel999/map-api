d2aff8a27cdfa1289ad8383d8e9242490667018b
HDaniel999 <hdaniel999@gmail.com>
2020-02-28 06:52:05 -0600

    Heroku config

--- config/index.js
+++ config/index.js
@@ -1,4 +1,6 @@
 module.exports = {
-    port: process.env.PORT || 8080,
-    database_url: process.env.STRING_CONNECTION || 'mongodb://localhost:8080'
+    port: process.env.PORT || 9000,
+    //8080
+    database_url: process.env.STRING_CONNECTION || 'mongodb+srv://admin:admin@parcomap-cluster-rogeh.gcp.mongodb.net/test?retryWrites=true&w=majority'
+    //'mongodb://localhost:8080'
 }
